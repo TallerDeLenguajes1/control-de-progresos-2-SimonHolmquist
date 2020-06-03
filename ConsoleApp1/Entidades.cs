1faac98c8569c5041de1503fa10c22b5151ab90e
Simón Holmquist <simon.holmquist@gmail.com>
2020-06-03 19:06:53 -0300

    ...

--- ConsoleApp1/Entidades.cs
+++ ConsoleApp1/Entidades.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.VisualBasic;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace ConsoleApp1
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Dynamic;
+using System.Linq;
 
 namespace ConsoleApp1
 {
@@ -7,7 +9,31 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Personaje pj = new Personaje();
+            int cantidad, jugador1, jugador2 , i;
+            const int MDP = 5000;
+            LinkedList<Personaje> personajesJugables = new LinkedList<Personaje>();
+            do
+            {
+                Console.WriteLine("¿Cuántos personajes desea crear? (Al menos 2)");
+                cantidad = Convert.ToInt32(Console.ReadLine());
+            } while (cantidad < 2);
+            for(i=0;i<cantidad;i++)
+            {
+                personajesJugables.AddLast(new Personaje());
+            }
+            while(personajesJugables.Count > 1)
+            {
+                i = 1;
+                Console.WriteLine("Elija 2 personajes para el siguiente combate");
+                foreach (Personaje pjs in personajesJugables)
+                {
+                    Console.WriteLine($"{i++}. {pjs.Nombre}");
+                }
+                Console.WriteLine("Jugador 1");
+                jugador1 = Convert.ToInt32(Console.ReadLine()) - 1;
+                Console.WriteLine("Jugador 2");
+                jugador2 = Convert.ToInt32(Console.ReadLine()) - 1;
+            }
         }
     }
 }
